97273343c26def2b461f2a43ad4265084e9fac82
mac160382 <mac160382@gmail.com>
2018-05-16 18:55:59 -0500

    Se quita la clase que facebook

--- resources/userRegistrationFacebook.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import reqparse, abort, Api, Resource
-
-class UserRegistrationFacebook(Resource):
-    def get(self, todo_id):    
-        return {'Id': todo_id}
-        
-    def post(self):
-        return {'message': 'User registration with Facebook'}
